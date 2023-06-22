210aad16d6ea9729c5467fb9d393c43f7612573e
ihar <iharrwe@gmail.com>
2023-06-22 12:20:31 +0300

    fix/replaced secret_word to env, change returned type validateUser function;

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,24 +2,26 @@ import { Injectable } from '@nestjs/common';
 import { compare } from 'bcrypt';
 import { sign } from 'jsonwebtoken';
 import { PrismaService } from '../prisma.service';
+import { User } from '../types';
+import * as process from 'process';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  async signPayload(email: string) {
-    return sign(email, 'secret', { expiresIn: '7d' });
+  async signPayload(user: User) {
+    return sign(user, process.env.SECRET_WORD, { expiresIn: '7d' });
   }
 
-  async validateUser(email: string, password: string): Promise<any> {
+  async validateUser(email: string, password: string): Promise<User | null> {
     const user = await this.prisma.users.findFirst({
       where: {
-        email: email,
+        email,
       },
     });
 
     if (!user) {
-      return false;
+      return null;
     }
 
     const isPasswordValid = await compare(password, user.password);
@@ -33,7 +35,7 @@ export class AuthService {
   async findByPayload(email: string) {
     const user = await this.prisma.users.findFirst({
       where: {
-        email: email,
+        email,
       },
     });
     if (!user) {
