902eda8b5c9d3e0222e8933b0039c8d41ae10923
ihar <iharrwe@gmail.com>
2023-06-23 09:30:12 +0300

    fix/change custom User type on Users type from prisma, update getAllOrders logic;

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,18 +2,18 @@ import { Injectable } from '@nestjs/common';
 import { compare } from 'bcrypt';
 import { sign } from 'jsonwebtoken';
 import { PrismaService } from '../prisma.service';
-import { User } from '../types';
 import * as process from 'process';
+import { Users } from '.prisma/client';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  async signPayload(user: User) {
+  async signPayload(user: Users) {
     return sign(user, process.env.SECRET_WORD, { expiresIn: '7d' });
   }
 
-  async validateUser(email: string, password: string): Promise<User | null> {
+  async validateUser(email: string, password: string): Promise<Users | null> {
     const user = await this.prisma.users.findFirst({
       where: {
         email,
--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -7,8 +7,8 @@ import { CreateOrderDto } from './dto/create-order-dto';
 export class OrderService {
   constructor(private prisma: PrismaService) {}
 
-  async getAllOrders(): Promise<Order[]> {
-    return await this.prisma.order.findMany({
+  async getAllOrders(): Promise<{ [key: string]: Order[] }[]> {
+    const orders = await this.prisma.order.findMany({
       include: {
         foods: {
           include: {
@@ -17,6 +17,19 @@ export class OrderService {
         },
       },
     });
+
+    return orders.reduce((acc, order) => {
+      const createdAt = order.createdAt.toISOString().slice(0, 10);
+      const existingOrder = acc.find((o) => Object.keys(o)[0] === createdAt);
+
+      if (existingOrder) {
+        existingOrder[createdAt].unshift(order);
+      } else {
+        acc.unshift({ [createdAt]: [order] });
+      }
+
+      return acc;
+    }, []);
   }
 
   async createOrder(orderDto: CreateOrderDto): Promise<Order> {
