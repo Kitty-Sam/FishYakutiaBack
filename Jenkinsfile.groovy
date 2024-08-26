03af53e2c6493fc485f17cfba803d593c8722ef9
KirillJBee <Jbeework@gmail.com>
2024-08-26 23:11:09 +0300

    prerelease

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -7,16 +7,12 @@ pipeline {
     environment {
         GIT_TOKEN = credentials('GIT_TOKEN')
         DOCKERHUB_CREDENTIALS = credentials('kirilljbee_dockerhub')
-        NAME_IMAGE = 'kirilljbee/FishYakutiaBack:latest'
         ENV = credentials('FISH.env')
     }
 
     stages {
         stage('Checkout') {
             steps {
-                // // Clean before build
-                // cleanWs()
-                // Checkout from SCM
                 checkout scmGit(
                                 branches: [[name: 'devops']],
                                 userRemoteConfigs: [[credentialsId:'GIT_TOKEN',
@@ -24,101 +20,34 @@ pipeline {
             }
         }
 
-        stage('Stop web server') {
-             steps {
-                  sh 'docker compose --env-file ${ENV} down'
-              }
-         }
+        // stage('Stop API server') {
+        //      steps {
+        //           sh 'docker compose --env-file ${ENV} down'
+        //       }
+        //  }
 
-        stage('Delete web server') {
-             steps {
-                  sh 'docker rmi fishyakutiabackkitty-front:latest'
-                  sh 'docker rmi fishyakutiabackkitty-backend:latest'
+        // stage('Delete API server') {
+        //      steps {
+        //           sh 'docker rmi fishyakutiabackkitty-backend:latest'
                   
-              }
-         }
+        //       }
+        //  }
 
-        stage('Start web server') {
-             steps {
-                  sh 'docker compose --env-file ${ENV} up -d'
-              }
-         }
+        // stage('Start API server') {
+        //      steps {
+        //           sh 'docker compose --env-file ${ENV} up -d'
+        //       }
+        //  }
 
-        
-
-
-        // stage('Push image webpage') {
-        //     steps {
-        //         sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
-        //         sh 'docker push ${NAME_IMAGE}'
-        //         sh 'docker system prune -af'
-        //     }
-        // }
-
-        // stage('Terraform init') {
-        //     steps {
-        //         sh 'terraform init'
-        //     }
-        // }
-
-        // stage('Terraform plan') {
-        //     steps {
-        //         sh 'terraform plan -out tfplan'
-        //         sh 'terraform show -no-color tfplan > tfplan.txt'
-        //     }
-        // }
-
-        // stage('Apply/Destroy') {
-        //     steps {
-        //         //sh 'terraform apply -input=false tfplan'
-        //         sh 'terraform destroy --auto-approve'
-        //     }
-        // }
-
-        // stage('Deploy webimage') {
-
-        //     steps {
-        //         script {
-        //             withCredentials([
-        //                 file(credentialsId: 'DH_vaultkey', variable: 'ANSIBLE_VAULT_KEY')
-        //                 ]) {
-        //                 sh 'ansible-playbook -i dynamic_inventory.ini --vault-password-file $ANSIBLE_VAULT_KEY playbook.yml'
-        //             }
-        //         }
-        //     }
-        // }
     }
 
-// post {
-//         // Clean after build
-//         always {
-//             cleanWs(cleanWhenNotBuilt: false,
-//                     deleteDirs: true,
-//                     disableDeferredWipeout: true,
-//                     notFailBuild: true,
-//                     patterns: [[pattern: '.gitignore', type: 'INCLUDE'],
-//                                [pattern: '.propsfile', type: 'EXCLUDE']])
-//         }
-//     }
-
-    // post {
-
-    //     success {
-    //         mail to: 'jbeework@gmail.com',
-    //         subject: "Job '${JOB_NAME}' (${BUILD_NUMBER}) was successfully completed!",
-    //         body: "Please go to ${BUILD_URL} and verify the build"
-    //     }
-
-    //     failure {
-    //         mail to: 'jbeework@gmail.com',
-    //         subject: "Job '${JOB_NAME}' (${BUILD_NUMBER}) ended unsuccessfully!",
-    //         body: "Please go to ${BUILD_URL} and verify the build"
-    //     }
-
-    //     aborted {
-    //         mail to: 'jbeework@gmail.com',
-    //         subject: "Job '${JOB_NAME}' (${BUILD_NUMBER}) was aborted",
-    //         body: "Please go to ${BUILD_URL} and verify the build"
-    //     }
-    // }
+    post {
+        always {
+            cleanWs()
+              dir("${env.WORKSPACE}@tmp") {
+                          deleteDir()
+              }
+        }
+    }
+  
 }
